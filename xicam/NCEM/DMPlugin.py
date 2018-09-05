78d6a280580443a34cc94d9fdded0c6918e19c63
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-09-04 21:12:08 -0700

    Add support for z, t stacked NCEM DM files

--- xicam/NCEM/DMPlugin.py
+++ xicam/NCEM/DMPlugin.py
@@ -9,15 +9,37 @@ import functools
 from pathlib import Path
 from ncempy.io import dm
 
+
+# TODO: ask peter to add __enter__ and 'with' support to NCEMPY's dm module
+
 class DMPlugin(DataHandlerPlugin):
     name = 'DMPlugin'
 
     DEFAULT_EXTENTIONS = ['.dm3', '.dm4']
 
-    def __call__(self, path, *args, **kwargs):
+    def __call__(self, path, index_z, index_t):
         aa = dm.fileDM(path)
         aa.parseHeader()
-        return aa.getDataset(kwargs.get('index', 0))['data']
+        return aa.getDataset(index_z)['data'][index_t]
+
+    @classmethod
+    def getEventDocs(cls, paths, descriptor_uid):
+        for path in paths:
+            for index_z in range(cls.num_z(path)):
+                for index_t in range(cls.num_t(path)):
+                    yield embedded_local_event_doc(descriptor_uid, 'primary', cls, (path, index_z, index_t))
+
+    @staticmethod
+    def num_z(path):
+        f = dm.fileDM(path)
+        f.parseHeader()
+        return f.zSize[0]
+
+    @staticmethod
+    def num_t(path):
+        f = dm.fileDM(path)
+        f.parseHeader()
+        return f.zSize[1]
 
     @staticmethod
     @functools.lru_cache(maxsize=10, typed=False)
